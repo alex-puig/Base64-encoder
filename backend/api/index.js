4684656dfc21382ba8ce7e2ddc56fb40641bf186
alex-puig <puig.mar0@gmail.com>
2026-01-06 17:13:06 -0600

    Add a runtime message to the API

--- backend/api/index.js
+++ backend/api/index.js
@@ -20,6 +20,10 @@ app.use('/api/base64', base64Routes);
 
 connectDB();
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.listen(PORT, () => {
   console.log(`Server started at port ${PORT}`);
 });
